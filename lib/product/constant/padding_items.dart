3f0a04a4ed0bbb561c2b64db1e3507e3c3fcd0ab
Cem Sarıkaya <cesadevolop@gmail.com>
2022-05-20 17:30:08 +0300

    phone_view done

---
+++ lib/product/constant/padding_items.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaddindUtility {
+  final paddingTop = const EdgeInsets.only(top: 10);
+  final paddinBottom = const EdgeInsets.only(bottom: 5, top: 2);
+  final paddingIcon = const EdgeInsets.only(right: 13);
+  final paddingCard = const EdgeInsets.all(8.0);
+  final paddingCardImage = const EdgeInsets.all(9.0);
+}
--- lib/view/phone_screen.dart
+++ lib/view/phone_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:phone_shop_app/model/phone_model.dart';
+import 'package:phone_shop_app/product/constant/padding_items.dart';
 import 'package:phone_shop_app/product/constant/project_dio.dart';
 import 'package:phone_shop_app/product/constant/project_items.dart';
 import 'package:phone_shop_app/product/widget/phone_slider.dart';
@@ -20,12 +22,20 @@ class _PhoneScreenViewState extends State<PhoneScreenView> with ProjectDioMixin 
 
   @override
   Widget build(BuildContext context) {
+    double sizedBoxHeight = MediaQuery.of(context).size.height / 1.2;
+
     return ChangeNotifierProvider(
         create: (context) => PhoneProvider(PhoneService(service)),
         builder: (context, child) {
           return Scaffold(
               appBar: AppBar(
                 title: const Text(ProjectItems.projectName),
+                actions: [
+                  Padding(
+                    padding: PaddindUtility().paddingIcon,
+                    child: const PhoneBasketBtn(),
+                  ),
+                ],
               ),
               body: ListView(
                 shrinkWrap: true,
@@ -34,46 +44,77 @@ class _PhoneScreenViewState extends State<PhoneScreenView> with ProjectDioMixin 
                     children: [
                       const PageUpContainer(),
                       SizedBox(
-                        height: MediaQuery.of(context).size.height / 1.2,
-                        child: GridView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                          ),
-                          itemCount: context.watch<PhoneProvider>().resources.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(2.0),
-                              child: Card(
-                                shadowColor: Colors.black,
-                                elevation: 5,
-                                shape:
-                                    const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(15.0),
-                                        child: Image.network(
-                                            context.watch<PhoneProvider>().resources[index].imageBlue.toString()),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(bottom: 15),
-                                      child: Text('${context.watch<PhoneProvider>().resources[index].prI}'),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
+                        height: sizedBoxHeight,
+                        child: _phoneGridView(context, context.watch<PhoneProvider>().resources),
                       ),
                     ],
                   )
                 ],
               ));
         });
+  }
+
+  GridView _phoneGridView(
+    BuildContext context,
+    List<PhoneModel> items,
+  ) {
+    Color cardShadowColor = Colors.black;
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: PaddindUtility().paddingCard,
+            child: Card(
+              shadowColor: cardShadowColor,
+              elevation: 5,
+              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: PaddindUtility().paddingCardImage,
+                      child: Image.network(items[index].imageBlue.toString()),
+                    ),
+                  ),
+                  Text(
+                    '${items[index].name}',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  Padding(
+                    padding: PaddindUtility().paddinBottom,
+                    child: Text(
+                      '${items[index].prI}',
+                      style: Theme.of(context).textTheme.subtitle1?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class PhoneBasketBtn extends StatelessWidget {
+  const PhoneBasketBtn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double iconSize = 30;
+    return IconButton(
+      icon: Icon(Icons.shopping_basket_sharp, size: iconSize),
+      onPressed: () {},
+    );
   }
 }
 
